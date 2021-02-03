5bbb853275cd851a50dc1a73389ffc6e9370a671
Thais Cristine <this.cristine06@gmail.com>
2021-02-03 19:40:41 +0200

    add: tooltip of errors

--- src/assets/components/Input/styles.ts
+++ src/assets/components/Input/styles.ts
@@ -1,8 +1,10 @@
 import styled, { css } from 'styled-components';
+import Tooltip from '../Tooltip';
 
 interface ContainerProps {
   isFocused: boolean;
   isFilled: boolean;
+  isErrored: boolean;
 }
 export const Container = styled.div<ContainerProps>`
   background: #232129;
@@ -13,6 +15,12 @@ export const Container = styled.div<ContainerProps>`
 
   border: 2px solid #232129;
   color: #666360;
+
+  ${props =>
+    props.isErrored &&
+    css`
+      color: #c53030;
+    `}
 
   ${props =>
     props.isFocused &&
@@ -43,5 +51,21 @@ export const Container = styled.div<ContainerProps>`
   }
   svg {
     margin-right: 16px;
+  }
+`;
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16px;
+  svg {
+    margin: 0;
+  }
+
+  span {
+    background: #c53030;
+    color: #fff;
+
+    &::before {
+      border-color: #c53030 transparent;
+    }
   }
 `;
